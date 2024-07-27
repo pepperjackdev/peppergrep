7fdad0c250a80c46709efad37bbd30225ec8ea9f
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-07-27 15:36:54 +0200

    Initialized the project

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use std::{error::Error, fs, vec};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments passed... please use `minigrep <query> <file>`");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config {query, file_path})
+    }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = vec![];
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
