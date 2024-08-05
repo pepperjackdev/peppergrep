b717d1c31530e3ffbe19fc4dbfc8d2820257417e
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-08-05 15:28:18 +0200

    Code refactor to improve readability with closures

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error, fs, vec};
+use std::{env, error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
@@ -23,23 +23,29 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, String> {
-        if args.len() < 3 {
-            return Err("not enough arguments passed... please use `minigrep <query> <file> [--ignore-case]`".to_string());
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, String> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string".to_string()),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get any source file string".to_string())
+        };
         
         let mut ignore_case = env::var("IGNORE_CASE").is_ok(); 
-
-        // if the fourth argument is provided and equals "--ignore-case", the env. variable is overrided
-        if args.len() > 3 {
-            let ignore_case_flag = &args[3];
-            if ignore_case_flag == "--ignore-case" {
+        if let Some(arg) = args.next() {
+            if arg == "--ignore-case" {
                 ignore_case = true;
+            } else if arg == "--attend-case" {
+                ignore_case = false;
             } else {
-                return Err(format!("invalid flag `{ignore_case_flag}`, try using --ignore-case instead."));
+                return Err(format!("invalid flag `{arg}`, try using --ignore-case instead."));
             }
         }
 
@@ -51,29 +57,19 @@ impl Config {
     }
 }
 
-pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = vec![];
-
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str>{
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
