cf5f09555a668a71a2ab00c5bbfb03375b41366b
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-08-08 12:41:32 +0200

    updated project name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, process};
 
-use minigrep::Config;
+use peppergrep::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
@@ -8,7 +8,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = peppergrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
